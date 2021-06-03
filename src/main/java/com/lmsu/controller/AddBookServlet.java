0cae3baa570c519f1d4dfbc9d4505731b6530cb6
nguyenNgN <ngianguyen81@gmail.com>
2021-06-03 14:43:50 +0700

    add id check function for bookDAO

---
+++ src/main/java/com/lmsu/controller/AddBookServlet.java
@@ -0,0 +1,18 @@
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddBookServlet", value = "/AddBookServlet")
+public class AddBookServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
