69e906b35ccaba32a59306ac6f5dcd7f477b700f
nguyenNgN <ngianguyen81@gmail.com>
2021-06-09 18:59:08 +0700

    added properties for CommentDTO

--- src/main/java/com/lmsu/comments/CommentDTO.java
+++ src/main/java/com/lmsu/comments/CommentDTO.java
@@ -1,7 +1,25 @@
 package com.lmsu.comments;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
-public class CommentDTO implements Serializable {
+public @Data
+class CommentDTO implements Serializable {
+    private String memberID;
+    private String bookID;
+    private String textComment;
+    private float rating;
+    private String editorID;
 
+    public CommentDTO() {
+    }
+
+    public CommentDTO(String memberID, String bookID, String textComment, float rating, String editorID) {
+        this.memberID = memberID;
+        this.bookID = bookID;
+        this.textComment = textComment;
+        this.rating = rating;
+        this.editorID = editorID;
+    }
 }
