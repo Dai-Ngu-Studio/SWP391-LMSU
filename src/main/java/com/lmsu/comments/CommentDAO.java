45ec60612123231e58bba1f4c848780f4e8a0b6b
nguyenNgN <ngianguyen81@gmail.com>
2021-06-09 18:45:51 +0700

    initialize CommentDAO, DTO

---
+++ src/main/java/com/lmsu/comments/CommentDAO.java
@@ -0,0 +1,4 @@
+package com.lmsu.comments;
+
+public class CommentDAO {
+}
---
+++ src/main/java/com/lmsu/comments/CommentDTO.java
@@ -0,0 +1,7 @@
+package com.lmsu.comments;
+
+import java.io.Serializable;
+
+public class CommentDTO implements Serializable {
+
+}
