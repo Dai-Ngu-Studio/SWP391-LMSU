0b60402c27e8beb1ad2d2e665b689c569d648603
nguyenNgN <ngianguyen81@gmail.com>
2021-06-09 18:25:00 +0700

    initialize comment servlet

---
+++ src/main/java/com/lmsu/controller/member/AddCommentServlet.java
@@ -0,0 +1,19 @@
+package com.lmsu.controller.member;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddCommentServlet", value = "/AddCommentServlet")
+public class AddCommentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
