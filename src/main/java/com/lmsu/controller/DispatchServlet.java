b1da2d0f3dfc15b77ee14f77af1457297db63e2a
phucnt31 <nguyenthienphuc.02@gmail.com>
2021-06-10 11:44:03 +0700

    Staff function worked

--- src/main/java/com/lmsu/controller/DispatchServlet.java
+++ src/main/java/com/lmsu/controller/DispatchServlet.java
@@ -31,11 +31,13 @@ public class DispatchServlet extends HttpServlet {
     private static final String CHANGE_PHONE_CONTROLLER = "ChangePhoneServlet";
     private static final String VIEW_BOOK_DETAILS_CONTROLLER = "ViewBookDetailsServlet";
     private static final String SEARCH_MEMBER_CONTROLLER = "SearchMemberServlet";
+    private static final String SEARCH_STAFF_CONTROLLER = "SearchStaffServlet";
     private static final String UPDATE_AUTHOR_CONTROLLER = "UpdateAuthorServlet";
     private static final String DELETE_AUTHOR_CONTROLLER = "DeleteAuthorServlet";
     private static final String SEARCH_AUTHOR_CONTROLLER = "SearchAuthorNameServlet";
     private static final String ADD_AUTHOR_CONTROLLER = "AddAuthorServlet";
     private static final String VIEW_AUTHOR_DETAILS_CONTROLLER = "ViewAuthorDetailsServlet";
+
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -82,6 +84,8 @@ public class DispatchServlet extends HttpServlet {
                 url = ADD_AUTHOR_CONTROLLER;
             } else if (button.equals("View Authors")){
                 url = VIEW_AUTHOR_DETAILS_CONTROLLER;
+            } else if (button.equals("SearchStaff")){
+                url = SEARCH_STAFF_CONTROLLER;
             }
         } finally {
             RequestDispatcher rd = request.getRequestDispatcher(url);
