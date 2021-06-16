936383902b9aa6e32ab344c548d5e65c0da5a39c
nguyenNgN <ngianguyen81@gmail.com>
2021-06-16 12:28:29 +0700

    added getdate utils

--- src/main/java/com/lmsu/utils/DateHelpers.java
+++ src/main/java/com/lmsu/utils/DateHelpers.java
@@ -1,12 +1,17 @@
 package com.lmsu.utils;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class DateHelpers implements Serializable {
 
-    public static final java.sql.Date getCurrentDate() {
-        java.util.Date currentDate = new java.util.Date();
-        java.sql.Date currentDateSQL = new java.sql.Date(currentDate.getTime());
-        return currentDateSQL;
+    public static java.sql.Date getCurrentDate() {
+        return java.sql.Date.valueOf(LocalDate.now());
+    }
+
+    public static java.sql.Date getDeadlineDate(java.sql.Date date, long days) {
+        LocalDate locDate = date.toLocalDate();
+        locDate = locDate.plusDays(days);
+        return java.sql.Date.valueOf(locDate);
     }
 }
