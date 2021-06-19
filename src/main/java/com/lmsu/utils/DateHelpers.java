79e9687b94fbe5df042aa7159a16ce0fbce9b478
nguyenNgN <ngianguyen81@gmail.com>
2021-06-19 13:06:03 +0700

    DateHelpers: use SQL Server timestamp

--- src/main/java/com/lmsu/utils/DateHelpers.java
+++ src/main/java/com/lmsu/utils/DateHelpers.java
@@ -1,17 +1,35 @@
 package com.lmsu.utils;
 
+import javax.naming.NamingException;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class DateHelpers implements Serializable {
 
-    public static java.sql.Date getCurrentDate() {
-        return java.sql.Date.valueOf(LocalDate.now());
-    }
+    public static java.sql.Date getCurrentDate() throws SQLException, NamingException {
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
 
-    public static java.sql.Date getDeadlineDate(java.sql.Date date, long days) {
-        LocalDate locDate = date.toLocalDate();
-        locDate = locDate.plusDays(days);
-        return java.sql.Date.valueOf(locDate);
+        try {
+            con = DBHelpers.makeConnection();
+            if (con != null) {
+                String sql = "SELECT CAST(CURRENT_TIMESTAMP AS DATE) AS [currentDate] ";
+                con.prepareStatement(sql);
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    return rs.getDate("currentDate");
+                }
+            }
+        } finally {
+            if (rs != null) rs.close();
+            if (stm != null) stm.close();
+            if (rs != null) con.close();
+        }
+        return null;
     }
 }
