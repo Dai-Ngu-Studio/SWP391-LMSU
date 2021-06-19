9c24ed356e2e14939a059572b1299dd1909b3f9a
ndungx <dpron12345@gmail.com>
2021-06-20 00:21:19 +0700

    change login email to login user id, remember me not done

--- src/main/java/com/lmsu/controller/LoginServlet.java
+++ src/main/java/com/lmsu/controller/LoginServlet.java
@@ -1,21 +1,24 @@
 package com.lmsu.controller;
 
+import com.google.common.hash.Hashing;
 import com.lmsu.users.UserDAO;
 import com.lmsu.users.UserDTO;
+import com.lmsu.utils.RandomStringHelpers;
 import org.apache.log4j.Logger;
 
 import javax.naming.NamingException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
 
     private static final String ERROR_PAGE = "login.html";
+    private static final String LOGIN_FAIL = "loginfail.html";
     private static final String INDEX_PAGE = "IndexServlet";
     private static final String DASHBOARD_PAGE = "dashboard.jsp";
     static final Logger LOGGER = Logger.getLogger(LoginServlet.class);
@@ -25,35 +28,43 @@ public class LoginServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
 
-        String email = request.getParameter("txtEmail");
+        String email = request.getParameter("txtUsername");
         String password = request.getParameter("txtPassword");
-        String isRemember = request.getParameter("cookie");
+        boolean isRememberMe = "true".equals(request.getParameter("cookie"));
         UserDAO dao = new UserDAO();
+        RandomStringHelpers random = new RandomStringHelpers();
 
         String url = ERROR_PAGE;
 
         try {
-            String[] mail = email.split("@");
+            String passwordHashed = Hashing.sha256()
+                    .hashString(password, StandardCharsets.UTF_8).toString();
 
-            if (mail[1].equalsIgnoreCase("fpt.edu.vn")) {
-                UserDTO dto = dao.checkLogin(email, password);
-                HttpSession session = request.getSession();
+            UserDTO dto = dao.checkLogin(email, passwordHashed);
+            HttpSession session = request.getSession();
 
-                if (isRemember != null) {
-                    Cookie cookie = new Cookie(mail[0], password);
-                    cookie.setMaxAge(60 * 60 * 24 * 15);
-                    response.addCookie(cookie);
+//            if (isRememberMe) {
+//                String selector = random.generatingRandomString();
+//                Cookie cookieSelector = new Cookie("selector", selector);
+//                cookieSelector.setMaxAge(60 * 60 * 24 * 7); //7 days
+//                response.addCookie(cookieSelector);
+//
+//                String validator = random.generatingRandomString();
+//                Cookie cookieValidator = new Cookie("validator", validator);
+//                cookieValidator.setMaxAge(60 * 60 * 24 * 7); //7 days
+//                response.addCookie(cookieValidator);
+//            }
+
+            if (dto != null) {
+                session.setAttribute("LOGIN_USER", dto);
+                if (dto.getRoleID().equals("4")) {
+                    url = INDEX_PAGE;
                 }
-
-                if (dto != null) {
-                    session.setAttribute("LOGIN_USER", dto);
-                    if (dto.getRoleID().equals("4")) {
-                        url = INDEX_PAGE;
-                    }
-                    if (dto.getRoleID().equals("1") || dto.getRoleID().equals("2") || dto.getRoleID().equals("3")) {
-                        url = DASHBOARD_PAGE;
-                    }
+                if (dto.getRoleID().equals("1") || dto.getRoleID().equals("2") || dto.getRoleID().equals("3")) {
+                    url = DASHBOARD_PAGE;
                 }
+            } else {
+                url = LOGIN_FAIL;
             }
         } catch (SQLException e) {
             LOGGER.error(e.getMessage());
