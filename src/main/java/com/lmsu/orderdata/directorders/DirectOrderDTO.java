5f70e6d8f554333379650a8c77006b328a17293e
nguyenNgN <ngianguyen81@gmail.com>
2021-06-24 00:46:50 +0700

    DirectOrderDTO: orderID to int

--- src/main/java/com/lmsu/orderdata/directorders/DirectOrderDTO.java
+++ src/main/java/com/lmsu/orderdata/directorders/DirectOrderDTO.java
@@ -7,14 +7,14 @@ import java.sql.Timestamp;
 
 public @Data
 class DirectOrderDTO implements Serializable {
-    private String orderID;
+    private int orderID;
     private String librarianID;
     private Timestamp scheduledTime;
 
     public DirectOrderDTO() {
     }
 
-    public DirectOrderDTO(String orderID, String librarianID, Timestamp scheduledTime) {
+    public DirectOrderDTO(int orderID, String librarianID, Timestamp scheduledTime) {
         this.orderID = orderID;
         this.librarianID = librarianID;
         this.scheduledTime = scheduledTime;
