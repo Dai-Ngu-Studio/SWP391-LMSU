63fce3d86236cb81341f08afb6c87b8e027694a2
nguyenNgN <ngianguyen81@gmail.com>
2021-06-23 21:28:24 +0700

    DirectOrderDAO: addOrder function

--- src/main/java/com/lmsu/orderdata/directorders/DirectOrderDAO.java
+++ src/main/java/com/lmsu/orderdata/directorders/DirectOrderDAO.java
@@ -1,4 +1,41 @@
 package com.lmsu.orderdata.directorders;
 
-public class DirectOrderDAO {
+import javax.naming.NamingException;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class DirectOrderDAO implements Serializable {
+    private Connection conn;
+
+    public DirectOrderDAO() {
+    }
+
+    public DirectOrderDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public boolean addDirectOrder(int orderID, Timestamp scheduledTime)
+            throws SQLException, NamingException {
+        PreparedStatement stm = null;
+        try {
+            if (conn != null) {
+                String sql = "INSERT INTO [DirectOrder] " +
+                        "([orderID], [scheduledTime]) " +
+                        "VALUES(?, ?) ";
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, orderID);
+                stm.setTimestamp(2, scheduledTime);
+                int row = stm.executeUpdate();
+                if (row > 0) {
+                    return true;
+                }
+            }
+        } finally {
+            if (stm != null) stm.close();
+        }
+        return false;
+    }
 }
