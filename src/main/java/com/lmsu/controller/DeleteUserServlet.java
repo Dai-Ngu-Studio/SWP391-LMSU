4d1ec83f00f220e0720077c8b0514b429939b2f4
ndungx <dpron12345@gmail.com>
2021-06-24 11:12:46 +0700

    Create DeleteUserServlet.java

---
+++ src/main/java/com/lmsu/controller/DeleteUserServlet.java
@@ -0,0 +1,61 @@
+package com.lmsu.controller;
+
+import com.lmsu.users.UserDAO;
+import org.apache.log4j.Logger;
+
+import javax.naming.NamingException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "DeleteUserServlet", value = "/DeleteUserServlet")
+public class DeleteUserServlet extends HttpServlet {
+
+    private static final String ERROR_PAGE = "error.jsp";
+    private static final String SHOW_MEMBER_CONTROLLER = "ShowMemberServlet";
+    private static final String SEARCH_USER_CONTROLLER = "SearchUserServlet";
+    static final Logger LOGGER = Logger.getLogger(DeleteUserServlet.class);
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        String url = ERROR_PAGE;
+        String userID = request.getParameter("userPk");
+        String searchValue = request.getParameter("txtSearchValue");
+
+        try {
+            UserDAO dao = new UserDAO();
+            boolean result = dao.deleteUser(userID);
+
+            if (result) {
+                if (searchValue == null || searchValue.trim().isEmpty()) {
+                    url = SHOW_MEMBER_CONTROLLER;
+                } else {
+                    url = SEARCH_USER_CONTROLLER;
+                }
+            }
+        } catch (SQLException ex){
+            LOGGER.error(ex.getMessage());
+            log("DeleteUserServlet _ SQL: " + ex.getMessage());
+        } catch (NamingException ex){
+            LOGGER.error(ex.getMessage());
+            log("DeleteUserServlet _ Naming: " + ex.getMessage());
+        } finally {
+            response.sendRedirect(url);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
