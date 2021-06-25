6cf6fa5bf425da4be0a0debbe61820f600eee694
nguyenNgN <ngianguyen81@gmail.com>
2021-06-25 23:02:25 +0700

    CommentServlet: update book rating

--- src/main/java/com/lmsu/controller/member/comment/EditCommentServlet.java
+++ src/main/java/com/lmsu/controller/member/comment/EditCommentServlet.java
@@ -1,5 +1,6 @@
 package com.lmsu.controller.member.comment;
 
+import com.lmsu.books.BookDAO;
 import com.lmsu.comments.CommentDAO;
 import com.lmsu.comments.CommentDTO;
 import com.lmsu.users.UserDTO;
@@ -11,6 +12,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "EditCommentServlet", value = "/EditCommentServlet")
 public class EditCommentServlet extends HttpServlet {
@@ -18,14 +20,23 @@ public class EditCommentServlet extends HttpServlet {
     static final Logger LOGGER = Logger.getLogger(EditCommentServlet.class);
     private static final String VIEW_BOOK_DETAILS_CONTROLLER = "ViewBookDetailsServlet";
 
+    private final boolean COMMENT_EDITED = true;
+    private final boolean COMMENT_NOT_DELETED = true;
+
+    private final String PARAM_BOOKID = "bookPk";
+    private final String PARAM_TXT_EDITCOMMENT = "txtEditComment";
+    private final String PARAM_COMMENT_MEMBER_ID = "commentMemberID";
+
+    private final String ATTR_LOGIN_USER = "LOGIN_USER";
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String bookID = request.getParameter("bookPk");
-        String textEditComment = request.getParameter("txtEditComment");
-        String memberID = request.getParameter("commentMemberID");
+        String bookID = request.getParameter(PARAM_BOOKID);
+        String textEditComment = request.getParameter(PARAM_TXT_EDITCOMMENT);
+        String memberID = request.getParameter(PARAM_COMMENT_MEMBER_ID);
 
-        String url = VIEW_BOOK_DETAILS_CONTROLLER + "?bookPk=" + bookID;
+        String url = VIEW_BOOK_DETAILS_CONTROLLER + "?" + PARAM_BOOKID + "=" + bookID;
 
         try {
             // 1. Check if session existed
@@ -34,13 +45,28 @@ public class EditCommentServlet extends HttpServlet {
             UserDTO userDTO = (UserDTO) session.getAttribute("LOGIN_USER");
             if (userDTO != null) {
                 String userID = userDTO.getId();
+                BookDAO bookDAO = new BookDAO();
                 CommentDAO commentDAO = new CommentDAO();
                 CommentDTO commentDTO = commentDAO.getCommentOfBookFromUserID(memberID, bookID);
                 // isEdited: true
                 boolean result = commentDAO.editBookComment(memberID, bookID, textEditComment,
-                        commentDTO.getRating(), userID, true, false);
-                if (result) {
-                    url = VIEW_BOOK_DETAILS_CONTROLLER + "?bookPk=" + bookID;
+                        commentDTO.getRating(), userID, COMMENT_EDITED, COMMENT_NOT_DELETED);
+                int numberOfRatingsGiven = 0;
+                int avgRating = 0;
+                commentDAO.viewBookComments(bookID);
+                List<CommentDTO> comments = commentDAO.getCommentList();
+                if (comments != null) {
+                    for (CommentDTO comment : comments) {
+                        if (!comment.isDeleteStatus()){
+                            float commentRating = comment.getRating();
+                            avgRating += (commentRating > 0) ? commentRating : 0;
+                            numberOfRatingsGiven += (commentRating > 0) ? 1 : 0;
+                        }
+                    }
+                    if (numberOfRatingsGiven > 0) {
+                        avgRating = avgRating / numberOfRatingsGiven;
+                        bookDAO.updateAvgRating(bookID, avgRating);
+                    }
                 }
             }
         } catch (SQLException e) {
