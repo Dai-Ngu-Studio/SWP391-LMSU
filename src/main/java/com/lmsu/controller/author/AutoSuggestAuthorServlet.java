67ec86feffc2ecc3f768a88cd009e411fdd1379e
unknown <hotiendat661@gmail.com>
2021-07-01 03:06:01 +0700

    AutoSuggestAuthorServlet: fix json format for autocomplete, send json to client

--- src/main/java/com/lmsu/controller/author/AutoSuggestAuthorServlet.java
+++ src/main/java/com/lmsu/controller/author/AutoSuggestAuthorServlet.java
@@ -11,17 +11,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 @WebServlet(name = "AutoSuggestAuthorServlet", value = "/AutoSuggestAuthorServlet")
 public class AutoSuggestAuthorServlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+    //code-fold
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-
         AuthorDAO dao = new AuthorDAO();
 
         try {
@@ -29,25 +29,16 @@ public class AutoSuggestAuthorServlet extends HttpServlet {
             //System.out.println(request.getParameter("query"));
             String q = request.getParameter("query");
             ArrayList<AuthorDTO> o = dao.getListAuthor(q);
-
             //convert java object to JSON format, and returned as JSON formatted string
             Gson gson = new Gson();
             String json = gson.toJson(o);
-            System.out.println(json);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"suggestions\":"+json+"}");
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (NamingException ex) {
             ex.printStackTrace();
         }
-    }
-    //code-fold
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
     }
 }
