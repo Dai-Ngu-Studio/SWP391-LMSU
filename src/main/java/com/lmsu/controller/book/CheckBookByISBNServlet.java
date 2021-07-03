d4d67929db02959e56d6b9ab2c6b33f28f1ec6c4
unknown <hotiendat661@gmail.com>
2021-07-04 02:41:52 +0700

    CheckBookByISBN: add restore a deleted book function

--- src/main/java/com/lmsu/controller/book/CheckBookByISBNServlet.java
+++ src/main/java/com/lmsu/controller/book/CheckBookByISBNServlet.java
@@ -8,8 +8,8 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/GetBookByISBNServlet")
-public class GetBookByISBNServlet extends HttpServlet {
+@WebServlet("/CheckBookByISBNServlet")
+public class CheckBookByISBNServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String isbnTen = request.getParameter("txtISBNTen");
