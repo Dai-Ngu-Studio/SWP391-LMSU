c27cc5d71aa854d1ce5423dee82cd70a03a3bd02
ndungx <dpron12345@gmail.com>
2021-07-22 15:23:53 +0700

    fix check null for notifyTenDaysLeft

--- src/main/java/com/lmsu/task/DailyTask.java
+++ src/main/java/com/lmsu/task/DailyTask.java
@@ -36,58 +36,60 @@ public class DailyTask extends TimerTask implements Serializable {
             //get return date
             Date returnDate = announcementDAO.getReturnDate();
 
-            //get today
-            Date today = new Date(System.currentTimeMillis());
+            if (returnDate != null) {
+                //get today
+                Date today = new Date(System.currentTimeMillis());
 
-            //get days between today -> return date
-            long daysBetween = ChronoUnit.DAYS.between(today.toLocalDate(), returnDate.toLocalDate());
+                //get days between today -> return date
+                long daysBetween = ChronoUnit.DAYS.between(today.toLocalDate(), returnDate.toLocalDate());
 
-            if (daysBetween == 10) {
-                Map<String, List<String>> map = userDAO.getUserWithBorrowedBooks();
-                if (map != null) {
-                    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-                        /**
-                         * @user[0] id
-                         * @user[1] name
-                         * @user[2] email
-                         */
-                        String[] user = entry.getKey().split(", ");
+                if (daysBetween == 10) {
+                    Map<String, List<String>> map = userDAO.getUserWithBorrowedBooks();
+                    if (map != null) {
+                        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                            /**
+                             * @user[0] id
+                             * @user[1] name
+                             * @user[2] email
+                             */
+                            String[] user = entry.getKey().split(", ");
 
-                        System.out.println("SSLEmail Start");
-                        Properties props = new Properties();
-                        props.put("mail.smtp.host", "smtp.gmail.com");
-                        props.put("mail.smtp.socketFactory.port", "465");
-                        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-                        props.put("mail.smtp.auth", "true");
-                        props.put("mail.smtp.port", "465");
+                            System.out.println("SSLEmail Start");
+                            Properties props = new Properties();
+                            props.put("mail.smtp.host", "smtp.gmail.com");
+                            props.put("mail.smtp.socketFactory.port", "465");
+                            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                            props.put("mail.smtp.auth", "true");
+                            props.put("mail.smtp.port", "465");
 
-                        Authenticator auth = new Authenticator() {
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(EMAIL, PASSWORD);
-                            }
-                        };
+                            Authenticator auth = new Authenticator() {
+                                protected PasswordAuthentication getPasswordAuthentication() {
+                                    return new PasswordAuthentication(EMAIL, PASSWORD);
+                                }
+                            };
 
-                        Session session = Session.getDefaultInstance(props, auth);
-                        System.out.println("Session created");
+                            Session session = Session.getDefaultInstance(props, auth);
+                            System.out.println("Session created");
 
-                        String subject = "[INFORMATION AND LIBRARY CENTER]_NOTIFY DUE DATE TO RETURN BOOKS";
+                            String subject = "[INFORMATION AND LIBRARY CENTER]_NOTIFY DUE DATE TO RETURN BOOKS";
 
-                        String body = "Dear " + user[1] + "<br>"
-                                + "<br>"
-                                + "Information and Library center remind you to return books before " + returnDate + "<br>"
-                                + "<br>"
-                                + String.join("<br>", entry.getValue()) + "<br>"
-                                + "<br>"
-                                + "Any mistake please reply this email.<br>"
-                                + "<br>"
-                                + "Thanks.<br>"
-                                + "Sincerely,<br>"
-                                + "Information and Library center";
+                            String body = "Dear " + user[1] + "<br>"
+                                    + "<br>"
+                                    + "Information and Library center remind you to return books before " + returnDate + "<br>"
+                                    + "<br>"
+                                    + String.join("<br>", entry.getValue()) + "<br>"
+                                    + "<br>"
+                                    + "Any mistake please reply this email.<br>"
+                                    + "<br>"
+                                    + "Thanks.<br>"
+                                    + "Sincerely,<br>"
+                                    + "Information and Library center";
 
-                        emailHelpers.sendEmail(session, user[2], subject, body);
-                    }//end for each user
-                }
-            }//end if days between = 10
+                            emailHelpers.sendEmail(session, user[2], subject, body);
+                        }//end for each user
+                    }
+                }//end if days between = 10
+            }//end if return date exist
         } catch (SQLException e) {
             LOGGER.error(e.getMessage());
             System.err.println("DailyTask _ SQL: " + e.getMessage());
@@ -102,7 +104,7 @@ public class DailyTask extends TimerTask implements Serializable {
             System.err.println("DailyTask _ Messaging: " + e.getMessage());
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
-            System.err.println("DailyTask _ Exception: " + e.getMessage() + "Announcement have not initialize");
+            System.err.println("DailyTask _ Exception: " + e.getMessage() + " (Announcement have not initialize)");
         }
     }
 
