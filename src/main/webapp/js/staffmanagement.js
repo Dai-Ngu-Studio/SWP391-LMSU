5cd9d98f5c9a772041bd7a52e2ee56b3365f837e
ndungx <dpron12345@gmail.com>
2021-07-25 19:12:18 +0700

    add staffmanagement.js

---
+++ src/main/webapp/js/staffmanagement.js
@@ -0,0 +1,11 @@
+function readURL(input, idImage) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#' + idImage)
+                .attr('src', e.target.result)
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
